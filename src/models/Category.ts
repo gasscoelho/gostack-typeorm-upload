c38690553497c17ee9fe6c5d332606005c99332d
BlaackRabbit <33602013+Potato@users.noreply.github.com>
2020-04-25 01:38:43 -0300

    feat: add Category model

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -1,10 +1,23 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('categories')
 class Category {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column('varchar')
   title: string;
 
+  @CreateDateColumn({ select: false })
   created_at: Date;
 
+  @UpdateDateColumn({ select: false })
   updated_at: Date;
 }
 
