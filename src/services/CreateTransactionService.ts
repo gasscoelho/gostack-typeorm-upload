e2a29e492c0f0aa570e76e8dd122bebd06837a86
BlaackRabbit <33602013+Potato@users.noreply.github.com>
2020-04-25 01:41:25 -0300

    feat: create service to save transactions in database

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,74 @@
-// import AppError from '../errors/AppError';
+import { getRepository, getCustomRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
 
+import Category from '../models/Category';
+
+import TransactionRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
+
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category: categoryTitle,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionRepository);
+
+    const categoryRepository = getRepository(Category);
+
+    const { total } = await transactionsRepository.getBalance();
+
+    // Check if type is income | outcome
+    if (!['income', 'outcome'].includes(type)) {
+      throw new AppError('type must be income or outcome');
+    }
+
+    // Check the balance before create a new transaction
+    if (type === 'outcome' && value > total) {
+      throw new AppError(
+        "It wasn't possible to create a new transaction. Please, check if you have enough credit before proceed with this action.",
+      );
+    }
+
+    const checkCategoryExist = await categoryRepository.findOne({
+      where: { title: categoryTitle },
+    });
+
+    let category_id = '';
+
+    // Check if category already exist
+    if (!checkCategoryExist) {
+      const category = categoryRepository.create({
+        title: categoryTitle,
+      });
+
+      await categoryRepository.save(category);
+
+      category_id = category.id;
+    } else {
+      category_id = checkCategoryExist.id;
+    }
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category_id,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
