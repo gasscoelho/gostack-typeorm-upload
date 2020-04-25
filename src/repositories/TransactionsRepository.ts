ff7d4727c0c4ef0d1ffebbe5110a32ed9882158c
BlaackRabbit <33602013+Potato@users.noreply.github.com>
2020-04-25 01:40:26 -0300

    feat: add custom Transaction repository

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -2,6 +2,10 @@ import { EntityRepository, Repository } from 'typeorm';
 
 import Transaction from '../models/Transaction';
 
+import groupBy from '../utils/array/groupBy';
+
+import isObjectEmpty from '../utils/object/isEmpty';
+
 interface Balance {
   income: number;
   outcome: number;
@@ -11,7 +15,41 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.find();
+
+    // Group Transactions by type (income | outcome)
+    const transactionsGroupByType = groupBy(transactions, 'type');
+    let income = 0;
+    let outcome = 0;
+    let total = 0;
+
+    // Check if object is empty
+    if (isObjectEmpty(transactionsGroupByType)) {
+      return { income, outcome, total };
+    }
+
+    // Calculate income | outcome
+    Object.entries(transactionsGroupByType).forEach(([key, value]) => {
+      switch (key) {
+        case 'income':
+          income = value.reduce((accumulator: number, currentValue) => {
+            return accumulator + +currentValue.value; // Force currentValue.value to number
+          }, 0);
+          break;
+        case 'outcome':
+          outcome = value.reduce((accumulator, currentValue) => {
+            return accumulator + +currentValue.value; // Force currentValue.value to number
+          }, 0);
+          break;
+        default:
+          break;
+      }
+    });
+
+    // Calculate the total value
+    total = income - outcome;
+
+    return { income, outcome, total };
   }
 }
 
