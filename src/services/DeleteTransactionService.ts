39babc16c5bb09c9741ac9e8465aa3e8d9e1aa22
BlaackRabbit <33602013+Potato@users.noreply.github.com>
2020-04-25 01:41:49 -0300

    feat: create service to delete a Transaction

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,26 @@
-// import AppError from '../errors/AppError';
+import { getRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+
+import Transaction from '../models/Transaction';
+
+interface Request {
+  id: string;
+}
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute({ id }: Request): Promise<void> {
+    const transactionsRepository = getRepository(Transaction);
+
+    const checkTransactionIdExist = await transactionsRepository.findOne({
+      where: { id },
+    });
+
+    // Check if ID Exist
+    if (!checkTransactionIdExist) {
+      throw new AppError('transaction not found.');
+    }
+
+    await transactionsRepository.delete(id);
   }
 }
 
