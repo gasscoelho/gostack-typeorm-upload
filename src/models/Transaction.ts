8bf9ebd2653657a7e08bf15c4c43f3da28c294fa
BlaackRabbit <33602013+Potato@users.noreply.github.com>
2020-04-25 01:38:59 -0300

    feat: add Transaction model

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,16 +1,40 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+
+import Category from './Category';
+
+@Entity('transactions')
 class Transaction {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column('varchar')
   title: string;
 
+  @Column('varchar')
   type: 'income' | 'outcome';
 
+  @Column('numeric')
   value: number;
 
+  @Column('varchar')
   category_id: string;
 
+  @ManyToOne(() => Category)
+  @JoinColumn({ name: 'category_id' })
+  category: Category;
+
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
